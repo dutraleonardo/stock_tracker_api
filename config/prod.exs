d0bfe6627d83ff2e94ea5a6b56fb582fc4ecc752
dutraleonardo <leonardo.uece@gmail.com>
2022-06-16 14:21:41 -0300

    remove cache manifest

--- config/prod.exs
+++ config/prod.exs
@@ -12,8 +12,7 @@ import Config
 config :stock_tracker_api, StockTrackerApiWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "finiam-stock-tracker-api.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
